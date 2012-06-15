f7252d70e7e4cbafe00f3a730cbffb38b21d034b
Kensuke Nagae <kyanny@gmail.com>
2012-06-15 13:15:32 +0900

    fix spec

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe "users/new" do
   before(:each) do
     assign(:user, stub_model(User,
-      :id => 1,
-      :name => "MyString",
-      :pass => "MyString",
-      :lastupdate => 1
+      :id                    => 1,
+      :name                  => "Richard Matthew Stallman",
+      :password              => "rms",
+      :password_confirmation => "rms",
     ).as_new_record)
   end
 
@@ -15,10 +15,9 @@ describe "users/new" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => users_path, :method => "post" do
-      assert_select "input#user_id", :name => "user[id]"
       assert_select "input#user_name", :name => "user[name]"
-      assert_select "input#user_pass", :name => "user[pass]"
-      assert_select "input#user_lastupdate", :name => "user[lastupdate]"
+      assert_select "input#user_password", :name => "user[password]"
+      assert_select "input#user_password_confirmation", :name => "user[password_confirmation]"
     end
   end
 end
